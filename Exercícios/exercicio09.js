57cce579cc5203796e6289a1f01bc072a5a7492b
GabrielaTPedroso <gabrielatpedroso@gmail.com>
2025-05-14 21:08:10 -0300

    Delete Exercícios directory

--- Exercícios/exercicio09.js
+++
@@ -1,6 +0,0 @@
-// 9. Implemente um programa que exibe uma contagem regressiva de 10 até 1 no console
-// utilizando um loop for.
-
-for (let contagem = 10; contagem >= 1; contagem--) {
-    console.log(contagem)
-}
--- Exercícios/exercicio10.js
+++
@@ -1,8 +0,0 @@
-// 10. Escreva um algoritmo para ler um número inteiro e escrevê-lo na tela 10 vezes.
-
-const prompt = require('prompt-sync')()
-
-let numero = Number(prompt('Insira um número inteiro:'))
-
-for (let contador = 1; contador <= 10; contador++)
-console.log(numero)
